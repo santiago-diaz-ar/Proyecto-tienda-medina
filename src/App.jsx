7f77387eb87b658de5262d82d63cc42c9f05d24f
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-10-10 19:19:57 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import {Route , Routes} from "react-router-dom"
+import Home from './components/home/home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>      
+  )
+}
+export default App
---
+++ src/components/home/home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from "./home.module.css";
+
+export default function home() {
+  return (
+    <div className={style.container}>
+      <div className={style.unoA}>
+        <div className={style.text}>
+          BIENVENIDO A LA TIENDA VIRTUAL LOS MEDINA
+        </div>
+      </div>
+      <div className={style.flex_container}>
+        <div className={style.cajas}>Productos alimenticios🍔</div>
+        <div className={style.cajas}>Productos para animales🐶</div>
+        <div className={style.cajas}>Productos de aseo🪥</div>
+        <div className={style.cajas}>Productos ?🤷​</div>
+      </div>
+    </div>
+  );
+}
