98a6bede42897e964236c865300a4f9c795c8075
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-10-13 10:14:44 -0500

    deployando en git pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import {Route , Routes} from "react-router-dom"
-import Home from './components/home/home'
+import { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/home/home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>      
-  )
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
-export default App
+export default App;
