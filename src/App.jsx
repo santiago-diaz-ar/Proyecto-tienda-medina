e59c9450a5196e28fd62e05bc77c273b3b8c4eae
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-10-14 19:21:23 -0500

    arreglos deploy git pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./components/home/home";
 function App() {
   return (
     <Routes>
-      <Route path="/Proyecto-tienda-medina" element={<Home />} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://santiago-diaz-ar.githud.io/Proyecto-tienda-medina",
+  base: "https://santiago-diaz-ar.githud.io",
   server: {
     proxy: {
       "/api": {
-        target: "https://santiago-diaz-ar.githud.io/Proyecto-tienda-medina", // Cambia esto a tu API
+        target: "https://santiago-diaz-ar.githud.io", // Cambia esto a tu API
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
