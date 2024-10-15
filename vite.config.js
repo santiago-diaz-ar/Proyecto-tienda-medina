1c239cf47f84c193da6a5762de0cadb827b34657
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-10-14 19:31:28 -0500

    error 1

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://santiago-diaz-ar.githud.io/Proyecto-tienda-medina",
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://santiago-diaz-ar.githud.io", // Cambia esto a tu API
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  base: "https://santiago-diaz-ar.github.io/Proyecto-tienda-medina",
 });
