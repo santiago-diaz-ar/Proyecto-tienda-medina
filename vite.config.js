e03bc611b5613fe884e0d3887486b119e9acb5c3
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-10-14 19:08:19 -0500

    correccion de deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://santiago-diaz-ar.githud.io/Proyecto-tienda-medina",
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://tu-api.com", // Cambia esto a tu API
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
