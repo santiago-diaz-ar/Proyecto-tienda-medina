5813cee2a8d8e1ee0c0d6f2555c7fabfcc6c108b
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-10-14 19:45:21 -0500

    arreglo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://santiago-diaz-ar.github.io/Proyecto-tienda-medina",
+  base: "/Proyecto-tienda-medina",
 });
