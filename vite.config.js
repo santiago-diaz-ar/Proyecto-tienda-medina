5ad9f4108c5c2862b0d99ff6f84d223f25f94e04
santiago-diaz-ar <santiagodiaz0201@gmail.com>
2024-10-14 19:26:34 -0500

    arreglos

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://santiago-diaz-ar.githud.io/Proyecto-tienda-medina", // Cambia esto a tu API
+        target: "https://santiago-diaz-ar.githud.io", // Cambia esto a tu API
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
